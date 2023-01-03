75346a88c5d8078b49d69894f4bb5050ccf0513d
xxx <25452644+bartoshevich@users.noreply.github.com>
2023-01-03 15:06:07 +0300

    feat: add categories and brands list to filter

--- src/app/components/filter.component.ts
+++ src/app/components/filter.component.ts
@@ -1,15 +1,42 @@
 import { DMComponent } from '../../frame/index';
 import { ComponentConfig } from '../../frame/tools/interfaces';
+import { copyProductList } from '../service/product-list';
+// import { Product } from '../service/product';
 
 class FilterComponent extends DMComponent {
   constructor(config: ComponentConfig) {
     super(config);
+    this.template = this.createFilterComponent();
   }
-}
 
-export const filterComponent = new FilterComponent({
-  selector: 'app-filter',
-  template: `
+  private objectCreate(arr: string[]): Record<string, number> {
+    const result: Record<string, number> = {};
+    arr.forEach( item => {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+    });
+    return result;
+  }
+
+  private categoriesList = (): Record<string, number> => {   
+    const categoriesList: string[] = [];
+    copyProductList.forEach( item => categoriesList.push(item.category));
+    categoriesList.sort((a, b) => a > b ? 1 : -1);
+    return this.objectCreate(categoriesList);
+  };
+
+  private brandList = (): Record<string, number> => {
+    const brandList: string[] = [];
+    copyProductList.forEach(item => brandList.push(item.brand));
+    brandList.sort((a, b) => a > b ? 1 : -1);
+    return this.objectCreate(brandList);
+  };
+  
+  private createFilterComponent(): string {
+    this.config.template = `
         <section class="filter__content">
           <h2 class="">Filter</h2>
           <section>
@@ -17,47 +44,47 @@ export const filterComponent = new FilterComponent({
               <summary>
               <h3 class="">Category</h3>
               </summary>
-                <ul> 
-                  <li class="filter__item">
-                    <label>
-                      <input type="checkbox"/>
-                      Mug
-                      <span>(2)</span>
-                    </label>
-                  </li>
-                  <li class="filter__item">
-                    <label>
-                      <input type="checkbox"/>
-                      Laptop
-                      <span>(4)</span>
-                    </label>
-                  </li>
-                </ul>
+                <ul>`;
+
+    for (const key in this.categoriesList()) {
+      this.config.template += `
+      <li class="filter__item">
+        <label>
+          <input type="checkbox"/>
+          ${key}
+          <span>(${this.categoriesList()[key]})</span>
+        </label>
+      </li>`;
+    }
+
+
+    this.config.template += `
+              </ul>
             </details>
             
           </section>
-
-
           <section>            
             <details>
               <summary>
               <h3 class="">Brand</h3>
               </summary>
-                <ul> 
-                  <li class="filter__item">
-                    <label>
-                      <input type="checkbox"/>
-                      System76
-                      <span>(2)</span>
-                    </label>
-                  </li>
-                  <li class="filter__item">
-                    <label>
-                      <input type="checkbox"/>
-                      Apple
-                      <span>(4)</span>
-                    </label>
-                  </li>
+                <ul> `;
+
+
+    for (const key in this.brandList()) {
+      this.config.template += `
+       <li class="filter__item">
+         <label>
+           <input type="checkbox"/>
+           ${key}
+           <span>(${this.brandList()[key]})</span>
+         </label>
+       </li>`;
+    }  
+
+
+
+    this.config.template += `
                 </ul>
             </details>
           </section>
@@ -97,6 +124,14 @@ export const filterComponent = new FilterComponent({
           </section>
 
         </section>
-    `,
+    `;
+    return this.config.template;
+  }
+
+}
+
+export const filterComponent = new FilterComponent({
+  selector: 'app-filter',
+  template: '',
   childComponents: [],
 });
