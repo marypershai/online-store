648461fc36fff2f1116ec49b6a3219927d82c6d5
Maryia Piarshai <mary.pershay@gmail.com>
2023-01-06 12:36:58 +0300

    feat: add personal data validation

--- src/app/components/order-modal.component.ts
+++ src/app/components/order-modal.component.ts
@@ -1,5 +1,6 @@
 import { ComponentConfig } from '../../frame/tools/interfaces';
 import { DMComponent } from '../../frame/index';
+import { checkEmail, checkLength, checkPhone, checkRequired } from '../../app/service/validation-form';
 
 
 class OrderModalComponent extends DMComponent {
@@ -15,26 +16,26 @@ class OrderModalComponent extends DMComponent {
         <form id="form" class="form">
           <h2>Personal Info</h2>
           <div class="form-control">
-              <label for="username">Username</label>
+              <label for="username">Name</label>
               <input type="text" id="username" name="username" placeholder="Enter username">
               <small>Username must be at least 3 characters</small>
+          </div>
+          <div class="form-control">
+            <label for="phone">Phone Number</label>
+            <input type="tel" id="phone" name="phone" placeholder="Enter phone number">
+            <small>You phone number is invalid</small>
+          </div>
+          <div class="form-control">
+            <label for="delivery">Delivery Address</label>
+            <input type="text" id="delivery" name="delivery" placeholder="Enter delivery address">
+            <small>You delivery address is invalid</small>
           </div>
           <div class="form-control">
               <label for="email">Email</label>
               <input type="text" id="email" name="email" placeholder="Enter email address">
               <small>Field cannot be empty</small>
           </div>
-          <div class="form-control">
-              <label for="password">Password</label>
-              <input type="password" id="password" name="password" placeholder="Enter password">
-              <small>Password must be 8 or more characters</small>
-          </div>
-          <div class="form-control">
-              <label for="password-confirmation">Confirm Password</label>
-              <input type="password" id="password-confirmation" name="password-confirmation" placeholder="Enter password again">
-              <small>Passwords must match</small>
-          </div>
-          <button type="submit">Submit</button>
+          <button type="submit" class="button button--buy button--order">Submit</button>
         </form>
       </div>
     `;
@@ -47,8 +48,18 @@ class OrderModalComponent extends DMComponent {
     };
   }
 
-  private validateForm() {
+  private validateForm(element: HTMLFormElement): void {
     console.log('validateForm');
+    element.preventDefault();
+    const username = document.querySelector('#username') as HTMLInputElement;
+    const email = document.querySelector('#email') as HTMLInputElement;
+    const delivery = document.querySelector('#delivery') as HTMLInputElement;
+    const phone = document.querySelector('#phone') as HTMLInputElement;
+    checkRequired([username, email, delivery, phone]);
+    checkEmail(email);
+    checkLength(username, 2, 3);
+    checkLength(delivery, 3, 5);
+    checkPhone(phone);
   }
 }
 
